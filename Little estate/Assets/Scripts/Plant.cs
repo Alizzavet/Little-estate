4538e60700a21b4c264c4d968397d5c964f70933
Alizzavet <alizzavet375@mail.ru>
2024-03-22 21:01:14 +0300

    Added some resources for a project

---
+++ Little estate/Assets/Scripts/Plant.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public abstract class Plant : MonoBehaviour
+{
+    protected Collider2D _collider;
+    protected float _cost;
+    protected Sprite _sprite;
+    
+    
+
+}
